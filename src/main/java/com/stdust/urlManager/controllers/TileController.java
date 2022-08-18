fb80fc09bcd298168808578d2135f4c654d3d069
St Dust <donteven@try.it>
2022-08-18 18:31:10 +0200

    URLManager: Implement request for Tile by its id

--- src/main/java/com/stdust/urlManager/controllers/TileController.java
+++ src/main/java/com/stdust/urlManager/controllers/TileController.java
@@ -5,8 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequestMapping("tiles")
@@ -22,5 +23,11 @@ public class TileController {
     public String index(Model model) {
         model.addAttribute("tiles", tileService.findAll());
         return "tiles/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("tile", tileService.findById(id));
+        return "tiles/show";
     }
 }
