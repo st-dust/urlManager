04fbe25b859ee45a9260de5c319fda3e237a2b65
St Dust <donteven@try.it>
2022-08-18 18:28:26 +0200

    URLManager: Fix Spring Security dependecies

--- src/main/java/com/stdust/urlManager/config/SecurityConfig.java
+++ src/main/java/com/stdust/urlManager/config/SecurityConfig.java
@@ -6,12 +6,15 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
-//@EnableGlobalMethodSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity security) throws Exception
     {
-        security.httpBasic().disable();
+        security.httpBasic().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/home", "/js/**", "/css/**").permitAll();
+//                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll();
     }
 }
