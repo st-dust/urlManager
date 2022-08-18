983575be54bf7033ac6f9dd8359f72e8cf22539f
St Dust <donteven@try.it>
2022-08-18 04:55:27 +0200

    URLManager: Initial commit

---
+++ src/test/java/com/stdust/urlManager/UrlManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stdust.urlManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
