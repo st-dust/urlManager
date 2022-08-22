418cc8b640b7d0ae3296744ae00a29756814aa14
St Dust <donteven@try.it>
2022-08-23 00:58:01 +0200

    URLManager: Set Collection details block and new collection button to max width

--- src/main/java/com/stdust/urlManager/config/SecurityConfig.java
+++ src/main/java/com/stdust/urlManager/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.stdust.urlManager.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,5 +17,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/", "/home", "/js/**", "/css/**").permitAll();
 //                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll();
+//        security
+//                //Configure AUTHORIZATION
+//
+//                .authorizeRequests()
+////                .antMatchers("/admin").hasRole("ADMIN")
+//                .antMatchers("/auth/login", "/error", "/auth/registration").permitAll()
+//                .anyRequest().hasAnyRole("USER", "ADMIN")
+//                .and()
+//
+//                //Configure login page
+////                .formLogin().loginPage("/auth/login")
+////                .loginProcessingUrl("/process_login")
+////                .defaultSuccessUrl("/hello", true)
+////                .failureUrl("/auth/login?error")
+//
+//                //Configure logout
+////                .and()
+//
+//                .logout()
+//                .logoutUrl("/logout")
+//                .logoutSuccessUrl("/auth/login");
     }
 }
