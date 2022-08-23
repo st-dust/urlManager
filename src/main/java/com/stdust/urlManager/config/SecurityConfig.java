eda575ffc08855129462de6a5186c80394330135
St Dust <donteven@try.it>
2022-08-23 02:00:56 +0200

    URLManager: Add custom login page

--- src/main/java/com/stdust/urlManager/config/SecurityConfig.java
+++ src/main/java/com/stdust/urlManager/config/SecurityConfig.java
@@ -1,42 +1,51 @@
 package com.stdust.urlManager.config;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import com.stdust.urlManager.service.PersonDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final PersonDetailsService personDetailsService;
+
+    @Autowired
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
+    }
 
     @Override
-    protected void configure(HttpSecurity security) throws Exception
-    {
-        security.httpBasic().disable()
-                .authorizeRequests()
-                .antMatchers("/", "/home", "/js/**", "/css/**").permitAll();
-//                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll();
-//        security
-//                //Configure AUTHORIZATION
-//
-//                .authorizeRequests()
-////                .antMatchers("/admin").hasRole("ADMIN")
-//                .antMatchers("/auth/login", "/error", "/auth/registration").permitAll()
-//                .anyRequest().hasAnyRole("USER", "ADMIN")
-//                .and()
-//
-//                //Configure login page
-////                .formLogin().loginPage("/auth/login")
-////                .loginProcessingUrl("/process_login")
-////                .defaultSuccessUrl("/hello", true)
-////                .failureUrl("/auth/login?error")
-//
-//                //Configure logout
-////                .and()
-//
-//                .logout()
-//                .logoutUrl("/logout")
-//                .logoutSuccessUrl("/auth/login");
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/auth/login", "/auth/registration", "/error").permitAll()
+                .anyRequest().hasAnyRole("USER", "ADMIN")
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello", true)
+                .failureUrl("/auth/login?error")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/auth/login");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService)
+                .passwordEncoder(getPasswordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
